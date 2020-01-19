646597f10ab83a862c0b019ddfb680c0975a0db0
LeshchenkoB <lba84@mail.ru>
2020-01-19 11:14:54 +0700

    добавил значение по умолчанию для аргументов функции renderGoodsItem. Добавил еще один элемент массива goods

--- main.js
+++ main.js
@@ -3,9 +3,10 @@ const goods = [
     { title: "Samsung Galaxy", price: 21500, img: 'https://via.placeholder.com/150' },
     { title: "Стиральная машина hotpoint", price: 32000, img: 'https://via.placeholder.com/150' },
     { title: "Умные часы apple watch", price: 26000, img: 'https://via.placeholder.com/150' },
+    { title: "Смартфон apple X", price: 56000, img: 'https://via.placeholder.com/150' }
 ];
 
-const renderGoodsItem = (title, price, img = '') => {
+const renderGoodsItem = (title = "Робот-пылесос xiaomi" , price = 20000, img = '') => {
     return `<div class="goods-item">
         <img src="${img}" alt="alt">
         <h3>${title}</h3>
