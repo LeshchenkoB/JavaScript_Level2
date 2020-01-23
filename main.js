5494380a0a87868e708cd4415d23c7ee1be777f5
LeshchenkoB <lba84@mail.ru>
2020-01-23 18:44:55 +0700

    Добавил пустые классы для корзины и элемента корзины

--- main.js
+++ main.js
@@ -1,23 +1,77 @@
-const goods = [
-    { title: "Робот-пылесос xiaomi", price: 20000, img: 'https://via.placeholder.com/150' },
-    { title: "Samsung Galaxy", price: 21500, img: 'https://via.placeholder.com/150' },
-    { title: "Стиральная машина hotpoint", price: 32000, img: 'https://via.placeholder.com/150' },
-    { title: "Умные часы apple watch", price: 26000, img: 'https://via.placeholder.com/150' },
-    { title: "Смартфон apple X", price: 56000, img: 'https://via.placeholder.com/150' }
-];
+/*
+* Класс для корзины товаров
+* */
+class GoodsBasket {
 
-const renderGoodsItem = (title = "Робот-пылесос xiaomi" , price = 20000, img = '') => {
-    return `<div class="goods-item">
-        <img src="${img}" alt="alt">
-        <h3>${title}</h3>
-        <p>${price}</p>
-        <button class="cart-button add-button" type="button">Добавить</button>
-    </div>`
-};
+}
+/*
+* Класс для товара корзины
+* */
+class GoodsBasketItem {
 
-const renderGoodsList = (list, container) => {
-    const goodsList = list.map(good => renderGoodsItem(good.title, good.price, good.img));
-    document.querySelector(container).innerHTML = goodsList.join("");
-};
+}
 
-renderGoodsList(goods, '.goods-list');
+
+class GoodsItem {
+    constructor(id, title = 'Без названия', price = 0, img = '') {
+        this.id = id;
+        this.title = title;
+        this.price = price;
+        this.img = img;
+    }
+    render() {
+        return `
+            <div class="goods-item" data-id="${this.id}">
+                <img src="${this.img}" alt="alt">
+                <h3>${this.title}</h3>
+                <p>${this.price}</p>
+                <button class="cart-button add-button" type="button">Добавить</button>
+            </div>
+        `;
+    }
+}
+class GoodsList {
+    constructor(container) {
+        this.container = document.querySelector(container);
+        this.goods = [];
+    }
+    initListeners() {
+        const buttons = [...this.container.querySelectorAll('.add-button')];
+        buttons.forEach(button => {
+            button.addEventListener('click', (event) => {
+                const goodId = event.target.parentElement.getAttribute('data-id');
+                this.addToCart(parseInt(goodId, 10));
+            })
+        })
+    }
+    findGood(id) {
+        return this.goods.find(good => good.id === id);
+    }
+    addToCart(goodId) {
+        const good = this.findGood(goodId);
+        console.log(good);
+    }
+    fetchGoods() {
+        this.goods = [
+            {id: 1, title: "Робот-пылесос xiaomi", price: 20000, img: 'https://via.placeholder.com/150'},
+            {id: 2, title: "Samsung Galaxy", price: 21500, img: 'https://via.placeholder.com/150'},
+            {id: 3, title: "Стиральная машина hotpoint", price: 32000, img: 'https://via.placeholder.com/150'},
+            {id: 4, title: "Умные часы apple watch", price: 26000, img: 'https://via.placeholder.com/150'},
+            {id: 5, title: "Посудомоечная машина bosh", price: 26000, img: 'https://via.placeholder.com/150'},
+        ]
+    }
+    render() {
+        let listHtml = '';
+        this.goods.forEach(good => {
+            const goodItem = new GoodsItem(good.id, good.title, good.price, good.img);
+            listHtml += goodItem.render();
+        });
+        this.container.innerHTML = listHtml;
+        this.initListeners();
+    }
+}
+
+const list = new GoodsList('.goods-list');
+list.fetchGoods();
+list.render();
+
