ae4d4212db62f960cebafe2ee658a490d26c25cd
LeshchenkoB <lba84@mail.ru>
2020-01-26 18:22:59 +0700

    сделал первое задание

--- main.js
+++ main.js
@@ -1,35 +1,37 @@
 const API_URL = "https://raw.githubusercontent.com/GeekBrainsTutorial/online-store-api/master/responses";
-function makeGetRequest(url, callback) {
-    let xhr;
-    if (window.XMLHttpRequest) {
-        xhr = new window.XMLHttpRequest();
-    } else  {
-        xhr = new window.ActiveXObject("Microsoft.XMLHTTP")
-    }
 
-    xhr.onreadystatechange = function () {
-        if (xhr.readyState === 4) {
-            callback(xhr.responseText)
-
+function makeGetRequest(url) {
+    return new Promise((resolve, reject) => {
+        let xhr;
+        if (window.XMLHttpRequest) {
+            xhr = new window.XMLHttpRequest();
+        } else  {
+            xhr = new window.ActiveXObject("Microsoft.XMLHTTP")
         }
-    };
 
-    xhr.open('GET', url);
-    xhr.send();
+        xhr.onreadystatechange = function () {
+            if (xhr.readyState === 4) {
+                resolve(xhr.responseText)
+            }
+        };
+
+        xhr.open('GET', url);
+        xhr.send();
+    });
 }
 
 class GoodsItem {
-    constructor(id, title = 'Без названия', price = 0, img = 'https://via.placeholder.com/200') {
-        this.id = id;
-        this.title = title;
+    constructor(id_product, product_name = 'Без названия', price = 0, img = 'https://via.placeholder.com/200') {
+        this.id_product = id_product;
+        this.product_name = product_name;
         this.price = price;
         this.img = img;
     }
     render() {
         return `
-            <div class="goods-item" data-id="${this.id}">
+            <div class="goods-item" data-id="${this.id_product}">
                 <img src="${this.img}" alt="alt">
-                <h3>${this.title}</h3>
+                <h3>${this.product_name}</h3>
                 <p>${this.price}</p>
                 <button class="cart-button add-button" type="button">Добавить</button>
             </div>
@@ -55,16 +57,11 @@ class GoodsList {
             }
         }
         return sum;
-        // return this.goods.reduce((totalPrice, good) => {
-        //     if (!good.price) return totalPrice;
-        //     totalPrice += good.price;
-        //     return totalPrice;
-        // }, 0)
     }
     render() {
         let listHtml = '';
         this.goods.forEach(good => {
-            const goodItem = new GoodsItem(good.id, good.product_name, good.price, good.img);
+            const goodItem = new GoodsItem(good.id_product, good.product_name, good.price, good.img);
             listHtml += goodItem.render();
         });
         this.container.innerHTML = listHtml;
@@ -83,10 +80,11 @@ class GoodsPage extends GoodsList {
         })
     }
     fetchGoods(callback) {
-        makeGetRequest(`${API_URL}/catalogData.json`, (goods) => {
+        makeGetRequest(`${API_URL}/catalogData.json`).then((goods) => {
             this.goods = JSON.parse(goods);
+        }).then(()=>{
             callback();
-        })
+        });
     }
     addToCart(goodId) {
         const good = this.findGood(goodId);
@@ -101,7 +99,7 @@ class Cart extends GoodsList {
     cleanCart() {
 
     }
-    updateCartItem(goodId, goods) {
+    updateCartItem() {
 
     }
 }
@@ -122,6 +120,8 @@ class CartItem extends GoodsItem {
 const list = new GoodsPage('.goods-list');
 list.fetchGoods(() => {
     list.render();
+    console.log(list.totalSum());
 });
 
-console.log(list.totalSum());
+const cart = new Cart;
+cart.updateCartItem();
