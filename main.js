3ff8630c14ea463f705c9ef5b93e5f7b85a97adc
LeshchenkoB <lba84@mail.ru>
2020-02-13 20:29:51 +0700

    выполнил первое задание урока№7

--- main.js
+++ main.js
@@ -19,32 +19,6 @@ function debounce(callback, wait, immediate) {
     }
 }
 
-Vue.component('cart', {
-    data:()=>({
-        cartGoods: [],
-    }),
-    template:`
-        <ol class="cart-goods">
-            <li v-for="good in cartGoods">{{ good.name }} Цена:{{ good.price }}руб. Кол-во {{good.counts}}</li>
-        </ol>
-    `,
-    computed: {
-        /*
-        * метод добавляет в корзину выбранные товары и считает их там
-        * */
-        addToCart(good){
-            console.log(good);
-            if (this.cartGoods.indexOf(good) == -1) {
-                this.cartGoods.push(good);
-                good.counts = 1
-            }
-            else{
-                good.counts += 1
-            }
-        }
-    }
-});
-
 Vue.component('goods-search',{
     data:()=>({
         searchLine: '',
@@ -58,12 +32,17 @@ Vue.component('goods-search',{
 
 Vue.component('goods-item', {
     props: ['good'],
+    methods: {
+        addToCart(){
+            return this.$emit('add', this.good);
+        }
+    },
     template: `
         <div class="goods-item" >
            <img :src="good.img" alt="alt">
            <h3>{{ good.name }}</h3>
            <p>{{ good.price }}</p>
-           <button @click="$emit('addToCart(good)')">Добавить</button>
+           <button @click="addToCart">Добавить</button>
         </div>
     `
 });
@@ -75,9 +54,14 @@ Vue.component('goods-list', {
             return this.goods.length === 0;
         },
     },
+    methods: {
+        addToCart(good){
+            return this.$emit('add', good);
+        }
+    },
     template: `
         <div class="goods-list" v-if="!isFilteredGoodsEmpty">
-            <goods-item v-for="good in goods"
+            <goods-item v-for="good in goods" @add="addToCart"
                         :key="good.id" :good="good"></goods-item>
         </div>
         <div class="goods-not-found" v-else>
@@ -134,11 +118,47 @@ const app = new Vue({
                 xhr.send(); // readyState 2
             });
         },
+        makePostRequest(url, data) {
+            return new Promise((resolve, reject) => {
+                let xhr;
+                if (window.XMLHttpRequest) {
+                    xhr = new window.XMLHttpRequest(); // readyState = 1
+                } else  {
+                    xhr = new window.ActiveXObject("Microsoft.XMLHTTP")
+                }
+
+                xhr.onreadystatechange = function () { // xhr changed
+                    if (xhr.readyState === 4) {
+                        if (xhr.status !== 200) {
+                            reject(xhr.responseText);
+                            return
+                        }
+                        const body = JSON.parse(xhr.responseText);
+                        resolve(body)
+                    }
+                };
+
+                xhr.onerror = function (err) {
+                    reject(err)
+                };
+
+                xhr.open('POST', url);
+                xhr.setRequestHeader('content-type', 'application/json');
+                xhr.send(JSON.stringify(data)); // readyState 2
+            });
+        },
         async fetchGoods() {
             try {
                 this.goods = await this.makeGetRequest(`/api/goods`);
                 this.filteredGoods = [...this.goods];
             } catch (e) {
+                console.error(e);
+            }
+        },
+        async addToCart(good){
+            try{
+                this.makePostRequest('/api/cart', good);
+            }catch (e) {
                 console.error(e);
             }
         },
