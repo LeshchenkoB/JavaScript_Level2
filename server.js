6b4bb6525936b1d67ae1023cb30d9945a3c8f4ca
LeshchenkoB <lba84@mail.ru>
2020-02-13 21:21:42 +0700

    3-е задание к уроку №7

--- server.js
+++ server.js
@@ -7,6 +7,23 @@ const app = express();
 app.use(express.static('.'));
 app.use(bodyParser.json());
 
+const ACTION_TYPES={
+    add: 'add',
+    remove: 'remove'
+};
+
+const log =(type, goodName)=>{
+    fs.readFile('./data/stats.json', 'utf-8', (err, data)=>{
+        const stats = JSON.parse(data);
+        stats.push({
+            type,
+            name: goodName,
+            createAt: +new Date()
+        });
+        fs.writeFile('./data/stats.json'. JSON.stringify(stats), () => {})
+    })
+};
+
 app.get('/api/goods', (req, res) => {
     fs.readFile('./data/catalog.json', 'utf-8', (err, data) => {
         res.send(data);
@@ -24,6 +41,7 @@ app.post('/api/cart', (req, res) => {
                 res.status(500).send();
                 return
             }
+            log(ACTION_TYPES.add, goodItem.name);
             res.send(cart);
         })
     });
@@ -33,13 +51,16 @@ app.delete('/api/cart/:id', (req, res)=>{
     fs.readFile('./data/cart.json', 'utf-8', (err, data) =>{
         const cart = JSON.parse(data);
         const id = parseInt(req.param.id, 10);
-        const newCart = cart.filter(good => good.id !== id);
-        fs.writeFile('./data/cart.json', JSON.stringify(newCart), (err) => {
+        const goodIndex = cart.findIndex(good => good.id !== id);
+        const good = cart[goodIndex];
+        cart.splice(goodIndex, 1);
+        fs.writeFile('./data/cart.json', JSON.stringify(cart), (err) => {
             if (err) {
                 res.status(500).send();
                 return
             }
-            res.send(newCart);
+            log(ACTION_TYPES.remove, good.name);
+            res.send(cart);
         })
     })
 });
