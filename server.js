dffa72306a376b45d4e56b8a6cb1145a8c50610d
LeshchenkoB <lba84@mail.ru>
2020-02-13 20:44:05 +0700

    Выполнил 2-е задание к уроку №7

--- server.js
+++ server.js
@@ -29,6 +29,21 @@ app.post('/api/cart', (req, res) => {
     });
 });
 
+app.delete('/api/cart/:id', (req, res)=>{
+    fs.readFile('./data/cart.json', 'utf-8', (err, data) =>{
+        const cart = JSON.parse(data);
+        const id = parseInt(req.param.id, 10);
+        const newCart = cart.filter(good => good.id !== id);
+        fs.writeFile('./data/cart.json', JSON.stringify(newCart), (err) => {
+            if (err) {
+                res.status(500).send();
+                return
+            }
+            res.send(newCart);
+        })
+    })
+});
+
 app.listen(3000, function () {
     console.log('server is running on port 3000!');
 });
